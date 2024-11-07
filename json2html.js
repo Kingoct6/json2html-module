9aad23ad8c7704eb9e8c0c21d077e703abff6f05
Kingoct6 <96378675+Kingoct6@users.noreply.github.com>
2024-11-07 18:49:36 +0530

    Create json2html.js

---
+++ json2html.js
@@ -0,0 +1,22 @@
+export default function json2html(data) {
+  const headers = Array.from(new Set(data.flatMap(Object.keys)));
+
+  let html = `<table data-user="saiharshil6@gmail.com">`;
+  html += "<thead><tr>";
+
+  headers.forEach(header => {
+    html += `<th>${header}</th>`;
+  });
+  html += "</tr></thead>";
+  html += "<tbody>";
+  data.forEach(row => {
+    html += "<tr>";
+    headers.forEach(header => {
+      html += `<td>${row[header] !== undefined ? row[header] : ""}</td>`;
+    });
+    html += "</tr>";
+  });
+  html += "</tbody></table>";
+
+  return html;
+}
